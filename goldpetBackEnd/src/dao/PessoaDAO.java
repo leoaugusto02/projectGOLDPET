c220d59571d636e5a79f81c85f41ce106a197fb2
leoaugusto02 <leonidasaugusto02@hotmail.com>
2019-10-08 10:44:23 -0300

    removendo vo's não utilizados

--- goldpetBackEnd/src/dao/PessoaDAO.java
+++ goldpetBackEnd/src/dao/PessoaDAO.java
@@ -7,9 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import vo.Animais;
 import vo.Guardiao;
-import vo.Laudo;
 import vo.Pessoa;
 
 public class PessoaDAO {
