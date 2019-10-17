7634d8b6fca04e8a0f018cfe203d885eeab21422
leoaugusto02 <leonidasaugusto02@hotmail.com>
2019-10-17 08:02:31 -0300

    criando uma condição para o insert no dao

--- goldpetBackEnd/src/dao/PessoaDAO.java
+++ goldpetBackEnd/src/dao/PessoaDAO.java
@@ -37,18 +37,22 @@ public class PessoaDAO {
 
 	public boolean cadastrar(Pessoa p, String acao) throws SQLException {
 		
-		String sql, sql2;
+		String sql, sql2, sqlCondicao = null;
 		
+		if(acao == "Guardião") {
+			sqlCondicao = "INSERT INTO Guardiao VALUES(null, (SELECT codePerson FROM Pessoa ORDER BY codePerson DESC LIMIT 1), 0,\"Iniciante\",0,\"ativo\")";
+		}else {
+			sqlCondicao = "INSERT INTO Funcionario VALUES(null, (SELECT codePerson FROM Pessoa ORDER BY codePerson DESC LIMIT 1), 0, ?, \"ativo\")";
+		}
 		
 		sql = "DELIMITER // "
-				+ "CREATE TRIGGER IF NOT EXISTS cadGuard AFTER INSERT "
+				+ "CREATE TRIGGER IF NOT EXISTS cadastro AFTER INSERT "
 				+ "ON Pessoa "
 				+ "FOR EACH ROW "
 				+ "BEGIN "
-				+ "INSERT INTO Guardiao "
-				+ "VALUES(null, (SELECT codePerson FROM Pessoa ORDER BY codePerson DESC LIMIT 1), 0,\"INICIANTE\",0,\"ativo\"); "
-				+ "END// "
-				+ "DELIMITER;";
+				+ sqlCondicao
+				+ " END// "
+				+ "DELIMITER ;";
 		
 		
 		sql2 = "INSERT INTO Pessoa VALUES(NULL, ?, ?, ?, ?, 'Guardião', ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -56,21 +60,22 @@ public class PessoaDAO {
 
 		con = ConnectionDB.getConnection();
 		ps = con.prepareStatement(sql);
+		ps.setString(1, p.getFuncionario().getCargo());
 		ps = con.prepareStatement(sql2);
-		ps.setString(1, p.getApelido());
-		ps.setString(2, p.getP_nome());
-		ps.setString(3, p.getS_nome());
-		ps.setDate(4, new java.sql.Date(p.getNascimento().getTime()));
-		ps.setString(5, p.getSenha());
-		ps.setString(6, p.getEmail());
-		ps.setString(7, p.getCpf());
-		ps.setString(8, p.getGenero());
-		ps.setString(9, p.getRg());
-		ps.setString(10, p.getCep());
-		ps.setString(11, p.getReferencia());
-		ps.setString(12, p.getImgPerfil());
-		ps.setString(13, p.getTel1());
-		ps.setString(14, p.getTel2());
+		ps.setString(2, p.getApelido());
+		ps.setString(3, p.getP_nome());
+		ps.setString(4, p.getS_nome());
+		ps.setDate(5, new java.sql.Date(p.getNascimento().getTime()));
+		ps.setString(6, p.getSenha());
+		ps.setString(7, p.getEmail());
+		ps.setString(8, p.getCpf());
+		ps.setString(9, p.getGenero());
+		ps.setString(10, p.getRg());
+		ps.setString(11, p.getCep());
+		ps.setString(12, p.getReferencia());
+		ps.setString(13, p.getImgPerfil());
+		ps.setString(14, p.getTel1());
+		ps.setString(15, p.getTel2());
 		
 		return ps.executeUpdate() > 0;
 	}
