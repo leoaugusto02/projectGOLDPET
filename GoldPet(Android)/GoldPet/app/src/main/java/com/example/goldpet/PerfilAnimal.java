67e04862ccadd2b0914a85a16b8304e3c9af0bb9
leoaugusto02 <leonidasaugusto02@hotmail.com>
2019-11-26 08:48:43 -0200

    mng

--- GoldPet(Android)/GoldPet/app/src/main/java/com/example/goldpet/PerfilAnimal.java
+++ GoldPet(Android)/GoldPet/app/src/main/java/com/example/goldpet/PerfilAnimal.java
@@ -1,15 +1,16 @@
 package com.example.goldpet;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.goldpet.model.ConsumirWebService;
+import com.example.goldpet.model.vo.Animais;
 import com.levirs.example.goldpet.R;
 
 import org.json.JSONObject;
@@ -19,7 +20,7 @@ public class PerfilAnimal extends AppCompatActivity {
     TextView txtNomePet, txtEspecie, txtRaca, txtStatus, txtGenero, txtIdade, txtNomeVet, txtDataDiagnostico, txtBreveDiagnostico, txtDiagnosticoCompleto;
     Button btnAdotar, btnLaudo;
     ImageView ivAnimal;
-    String MY_PREFS_NAME;
+    private Handler handler;
 
 
     @Override
@@ -27,22 +28,44 @@ public class PerfilAnimal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.perfil_animal);
 
+        handler = new Handler();
+
         perfilAnimal();
     }
 
     private void perfilAnimal() {
         new Thread(){
             public void run(){
-                SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
                 int codeAnimal = getIntent().getIntExtra("codeAnimal" ,0);
 
-                JSONObject jsonPerfilAnimal = ConsumirWebService.perfilAnimal(codeAnimal);
+                final JSONObject jsonPerfilAnimal = ConsumirWebService.perfilAnimal(codeAnimal);
 
-                if(jsonPerfilAnimal != null){
-                    
+                if(jsonPerfilAnimal == null){
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(PerfilAnimal.this, "Perfil inexistente", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    });
+                }else{
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            exibirpet(jsonPerfilAnimal);
+                        }
+                    });
                 }
-
             }
         }.start();
+    }
+
+    private void exibirpet(JSONObject jsonObject) {
+        try{
+            txtNomePet.setText("ufs");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
