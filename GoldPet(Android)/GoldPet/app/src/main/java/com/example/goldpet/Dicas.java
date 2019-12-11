ac7df92779773e5d8b4026c44e7652682c342268
Tamaramp <pompeutamara@gmai.com>
2019-12-11 08:18:08 -0300

    yh

--- GoldPet(Android)/GoldPet/app/src/main/java/com/example/goldpet/Dicas.java
+++ GoldPet(Android)/GoldPet/app/src/main/java/com/example/goldpet/Dicas.java
@@ -1,14 +1,11 @@
 package com.example.goldpet;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.levirs.example.goldpet.MainActivity;
 import com.levirs.example.goldpet.R;
 
 public class Dicas extends AppCompatActivity implements View.OnClickListener {
