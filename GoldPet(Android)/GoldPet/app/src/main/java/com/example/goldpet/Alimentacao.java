7ad93e30d8490b458e13e7651ca11319e38a87b3
Tamaramp <pompeutamara@gmai.com>
2019-12-12 09:30:23 -0300

    xs

--- GoldPet(Android)/GoldPet/app/src/main/java/com/example/goldpet/Alimentacao.java
+++ GoldPet(Android)/GoldPet/app/src/main/java/com/example/goldpet/Alimentacao.java
@@ -8,71 +8,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.levirs.example.goldpet.R;
 
-public class Dicas extends AppCompatActivity implements View.OnClickListener {
+public class Alimentacao extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnAlimentacao, btnHigiene, btnPasseio, btnVeterinario, btnOlfato,btnDormir, btnCachorroFaz,btnBeijam, btnSuam,btnTruques;
+    Button btVoltar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dicas);
+        setContentView(R.layout.alimentacao);
 
-        btnAlimentacao = findViewById(R.id.btnAlimentacao);
-        btnHigiene = findViewById(R.id.btnHigiene);
-        btnPasseio = findViewById(R.id.btnPasseio);
-        btnVeterinario = findViewById(R.id.btnVeterinario);
-        btnOlfato = findViewById(R.id.btnOlfato);
-        btnDormir = findViewById(R.id.btnDormir);
-        btnCachorroFaz = findViewById(R.id.btnCachorroFaz);
-        btnBeijam = findViewById(R.id.btnBeijam);
-        btnSuam = findViewById(R.id.btnSuam);
-        btnTruques = findViewById(R.id.btnTruques);
-
-        btnAlimentacao.setOnClickListener(this);
-        btnHigiene.setOnClickListener(this);
-        btnPasseio.setOnClickListener(this);
-        btnVeterinario.setOnClickListener(this);
-        btnOlfato.setOnClickListener(this);
-        btnDormir.setOnClickListener(this);
-        btnCachorroFaz.setOnClickListener(this);
-        btnBeijam.setOnClickListener(this);
-        btnSuam.setOnClickListener(this);
-        btnTruques.setOnClickListener(this);
-
+        btVoltar = findViewById(R.id.btVoltar);
+        btVoltar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btnAlimentacao:
-                setContentView(R.layout.alimentacao);
-                break;
-            case R.id.btnHigiene:
-                setContentView(R.layout.higiene);
-                break;
-            case R.id.btnPasseio:
-                setContentView(R.layout.passeio);
-                break;
-            case R.id.btnVeterinario:
-                setContentView(R.layout.levaraoveterinario);
-                break;
-            case R.id.btnOlfato:
-                setContentView(R.layout.olfato);
-                break;
-            case R.id.btnDormir:
-                setContentView(R.layout.dormirpet);
-                break;
-            case R.id.btnCachorroFaz:
-                setContentView(R.layout.cachorrofazem);
-                break;
-            case R.id.btnBeijam:
-                setContentView(R.layout.adorambeijar);
-                break;
-            case R.id.btnSuam:
-                setContentView(R.layout.suam);
-                break;
-            case R.id.btnTruques:
-                setContentView(R.layout.truques);
+            case R.id.btVoltar:
+                setContentView(R.layout.dicas);
                 break;
         }
     }
